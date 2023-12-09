6d3e7488926419abc8620e60c6ba360a6803d675
maxime.andre <maxime.andr@gmail.com>
2023-12-09 09:03:05 +0100

    clean day9

--- day9/day9_test.py
+++ day9/day9_test.py
@@ -9,14 +9,6 @@ def test_example_part1():
         result = day9.part1(content)
         assert result == 114
 
-def test_example2_part1():
-    # Open file test and read lines
-    with open('./day9/ressources/test2') as f:
-        content = f.readlines()
-        result = day9.part1(content)
-        assert result == 114
-
-
 def test_part1():
     # Open file test and read lines
     with open('./day9/ressources/input') as f:
